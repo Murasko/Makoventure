bc66a152a803aba4f71347219610ac595a19de9a
Murasko <github@murasko.de>
2022-11-08 17:53:38 +0100

    start of mvp map functions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from src.creatures.player.player import Player
+from src.map.map import Map
 
 if __name__ == '__main__':
     name = input("Enter your name:\n> ")
     p = Player(name)
+    m = Map()
     while True:
         command = input("> ").lower()
-        if command == "forward":
-            pass
-        elif command == "attack":
-            pass
+        if command == "next":
+            m.next()
+        elif command == "help":
+            print("""Command help:
+help - Prints help
+next - move to next field""")
--- src/map/map.py
+++ src/map/map.py
@@ -3,14 +3,7 @@ from src.creatures.enemies.enemy import Enemy
 
 class Map:
     def __init__(self):
-        self.current_field = []
         pass
 
-    @property
-    def field(self):
-        return self.current_field
-
-    @field.setter
-    def field(self, enemy_amount):
-        for i in range(1, enemy_amount):
-            self.current_field.append(Enemy)
+    def next(self):
+        pass
